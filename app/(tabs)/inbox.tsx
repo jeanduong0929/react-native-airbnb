740f573d7a7df153167b554d4c6b0e43c986a03d
Jean Duong <baoduongxy@gmail.com>
2024-03-25 15:36:55 -0400

    tabs done

---
+++ app/(tabs)/inbox.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const InboxTab = () => {
+  return (
+    <View>
+      <Text>InboxTab</Text>
+    </View>
+  );
+};
+
+export default InboxTab;
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ExplorerTab = () => {
+  return (
+    <View>
+      <Text>ExplorerTab</Text>
+    </View>
+  );
+};
+
+export default ExplorerTab;
