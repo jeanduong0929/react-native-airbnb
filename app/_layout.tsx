c2fd372758c2b313f0f82395a95df759c50bae61
Jean Duong <baoduongxy@gmail.com>
2024-03-30 13:56:51 -0400

    Added clerk provider with get/save token cache

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,19 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-
-import { useColorScheme } from '@/components/useColorScheme';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import * as SecureStore from "expo-secure-store";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -22,7 +21,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -45,14 +44,47 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+  /**
+   * A token cache object that provides methods for getting and saving tokens.
+   */
+  const tokenCache = {
+    /**
+     * Retrieves a token from the cache.
+     * @param key - The key associated with the token.
+     * @returns A Promise that resolves to the token value, or null if the token is not found or an error occurs.
+     */
+    async getToken(key: string) {
+      try {
+        return await SecureStore.getItemAsync(key);
+      } catch (error: any) {
+        console.error(error);
+        return null;
+      }
+    },
+
+    /**
+     * Saves a token to the cache.
+     * @param key - The key associated with the token.
+     * @param value - The value of the token to be saved.
+     * @returns A Promise that resolves when the token is successfully saved, or throws an error if saving fails.
+     */
+    async saveToken(key: string, value: string) {
+      try {
+        return await SecureStore.setItemAsync(key, value);
+      } catch (error: any) {
+        console.error(error);
+      }
+    },
+  };
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+      tokenCache={tokenCache}
+    >
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
-    </ThemeProvider>
+    </ClerkProvider>
   );
 }
